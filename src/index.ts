a007c554dd5961905a8294575e0260b7a9b3b5b7
nanopx <0nanopx@gmail.com>
2016-02-14 22:55:41 +0900

    fixed configurations

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,11 @@
 const x: number = 1;
 
-function f(x: number, ...r: any[]) {
+function f(x: number, ...r: number[]): number {
+  if (r) {
+    return r[0];
+  }
+
   return x;
 }
 
-f(2);
+f(2, 1, 3, 4);
